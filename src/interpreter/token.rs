f617d4846d803c00ad610b0eb2adb838aeec83e5
usercommon <kirilnenashev@gmail.com>
2023-08-13 17:59:10 +0300

    Revert "2"
    This reverts commit 73f959fd90354d5117e6ef1175c4fc3c9abfb637.

--- src/interpreter/token.rs
+++ src/interpreter/token.rs
@@ -33,13 +33,14 @@ impl Display for TokenType {
 
 // literal: FIXME dont need it because enums can store values
 // and i assume, that u can just use enums...
-pub struct Token {
+pub struct Token<T: Display> {
 	token_type: TokenType,
 	lexeme: String,
+	literal: Option<T>,
 	line: u32,
 }
 
-impl Token {
+impl<T: Display> Token<T> {
 	pub fn new<S>(token_t: TokenType, lexeme: S, literal: Option<T>, line: u32) -> Self
 	where 
 		S: Into<String>	
@@ -48,15 +49,16 @@ impl Token {
 		Self {
 			token_type : token_t,
 			lexeme : lexeme.into(),
+			literal : literal,
 			line : line
 		}
 	}
 }
 
-impl Debug for Token {
+impl<T: Display> Debug for Token<T> {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		match self.literal {
-			Some(lit) => write!(f, "{} {} {}", self.token_type, self.lexeme, self.line),
+			Some(lit) => write!(f, "{} {} {} {}", self.token_type, self.lexeme, lit, self.line),
 			None => write!(f, "{} {} {} {}", self.token_type, self.lexeme, " ", self.line),
 		}
     }
